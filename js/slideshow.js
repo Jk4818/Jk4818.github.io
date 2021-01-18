d55f984930ff0ab308fc46bf9fbf06c4a3e2dd8f
Jk4818 <56597732+Jk4818@users.noreply.github.com>
2021-01-18 13:06:48 +0000

    Add files via upload

---
+++ js/slideshow.js
@@ -0,0 +1,26 @@
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1} 
+    x[slideIndex-1].style.display = "block"; 
+    setTimeout(carousel, 10000); 
+}
+
+function carousel() {
+    var i;
+    var y = document.getElementsByClassName("image_slide");
+    for (i = 0; i < y.length; i++) {
+    y[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > y.length) {slideIndex = 1} 
+    y[slideIndex-1].style.display = "block"; 
+    setTimeout(carousel, 2000); 
+}
