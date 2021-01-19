3fe52428063626825a7d6f4b3c0b15c6ee8a6e5b
Jk4818 <56597732+Jk4818@users.noreply.github.com>
2021-01-19 15:46:02 +0000

    Updated Animations

---
+++ js/app.js
@@ -0,0 +1,69 @@
+//Scroll to top button
+var scrollToTopBtn = document.getElementById("scrollToTopBtn")
+var rootElement = document.documentElement
+
+function scrollToTop() {
+  // Scroll to top logic
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+}
+scrollToTopBtn.addEventListener("click", scrollToTop)
+
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+/*---PIN NAV---*/
+ScrollTrigger.create({
+  trigger: ".h_wrapper",
+  start: "-50px top", 
+  end: "6000vh top",
+  pin: "#nav",
+  pinSpacing: false,
+});
+
+
+/*---SLIDE FADE---*/
+
+function animateFrom(elem, direction) {
+  direction = direction | 1;
+  
+  var x = 0,
+      y = direction * 100;
+  if(elem.classList.contains("gs_reveal_fromLeft")) {
+    x = -100;
+    y = 0;
+  } else if(elem.classList.contains("gs_reveal_fromRight")) {
+    x = 100;
+    y = 0;
+  }
+  gsap.fromTo(elem, {x: x, y: y, autoAlpha: 0}, {
+    duration: 1.25, 
+    x: 0,
+    y: 0, 
+    autoAlpha: 1, 
+    ease: "expo", 
+    overwrite: "auto"
+  });
+}
+
+function hide(elem) {
+  gsap.set(elem, {autoAlpha: 0});
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  gsap.registerPlugin(ScrollTrigger);
+  
+  gsap.utils.toArray(".gs_reveal").forEach(function(elem) {
+    hide(elem); // assure that the element is hidden when scrolled into view
+    
+    ScrollTrigger.create({
+      trigger: elem,
+      onEnter: function() { animateFrom(elem) }, 
+      onEnterBack: function() { animateFrom(elem, -1) },
+      onLeave: function() { hide(elem) } // assure that the element is hidden when scrolled into view
+    });
+  });
+});
