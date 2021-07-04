0f85035a6e53f758783afc8ef4c97b33c634bb40
Jason Keung <jasonkeung@Jasons-MacBook-Pro.local>
2021-07-04 13:34:51 +0100

    minified projects page

---
+++ js/carousel.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+			$('.customer-logos').slick({
+				slidesToShow: 4,
+				slidesToScroll: 1,
+				autoplay: true,
+				autoplaySpeed: 2500,
+				arrows: false,
+				dots: true,
+					pauseOnHover: true,
+					responsive: [{
+					breakpoint: 2000,
+					settings: {
+						slidesToShow: 3
+					}
+				}, {
+					breakpoint: 520,
+					settings: {
+						slidesToShow: 1,
+						centerMode: false, /* set centerMode to false to show complete slide instead of 3 */
+						slidesToScroll: 1
+					}
+				}, ]
+			});
+		});
